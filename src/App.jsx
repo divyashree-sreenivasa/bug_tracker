d9e71876a579a664f3d8556209232ad1b15ad0ca
Divyashree Sreenivasa <divyashree.sreenivasa@gmail.com>
2020-10-04 23:01:36 +0530

    Placeholders for bug display page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-const element = (
-    <div title="outer-div">
-        <h1>Bug Tracker</h1>
-    </div>
-);
+class BugFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the bug filter.</div>
+        );
+    }
+}
 
+class BugTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the table of bugs.</div>
+        );
+    }
+}
+
+class BugAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for a form to add a bug.</div>
+        );
+    }
+}
+
+class BugList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Bug Tracker</h1>
+                <BugFilter />
+                <hr />
+                <BugTable />
+                <hr />
+                <BugAdd />
+            </React.Fragment>
+        );
+    }
+}
+
+const element = <BugList />;
 ReactDOM.render(element, document.getElementById("content"));
